3f925aa6accabdd7eefba94f0eff354e27b7074a
Kalhum <268527488+671463036-kritsada@users.noreply.github.com>
2026-05-25 21:06:35 +0700

    change config

--- Ise-thailand/src/constants/uploads_url.js
+++ Ise-thailand/src/constants/uploads_url.js
@@ -1,3 +1,4 @@
 import config from '../config'
 
-export const UPLOADS_URL = `${config.BASE_URL}/api/uploads/`
+// export const UPLOADS_URL = `${config.BASE_URL}/api/uploads/`
+export const UPLOADS_URL = `${config.BASE_URL}/uploads/`
