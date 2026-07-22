2ab217f678f1cd722e31957d6f4063b9eefeb59c
Kalhum <129973108+Kalhum@users.noreply.github.com>
2026-07-22 10:49:31 +0700

    change path config

--- Ise-thailand/src/config.js
+++ Ise-thailand/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // BASE_URL: 'https://ise-thailand.org',
-   BASE_URL: 'http://localhost:2000',
+  BASE_URL: "https://ise-thailand.org",
+  //  BASE_URL: 'http://localhost:2000',
 };
 
 export default config;
